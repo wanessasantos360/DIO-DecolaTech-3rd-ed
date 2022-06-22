f24861890b041f53fbca85bf5e07260b2280f07f
Wanessa Santos <wanessa.ufpe2018@gmail.com>
2022-06-22 13:47:58 -0300

    Introdução a Funcoes

---
+++ introducao-ao-javascript/sintaxe-e-operadores_atividade/scripts.js
@@ -0,0 +1,23 @@
+var num1 
+var num2 
+
+// console.log(verifica_igual(num1,num2))
+
+function verifica_igual (num1, num2){
+  if (num1 == num2){
+    return true
+  }
+
+  else{
+    return false
+  }
+}
+
+function soma_maior10(num1,num2){
+  soma = num1+num2
+  if (soma >= 10 || soma <=20 ){
+    result = soma > 10 || soma < 20
+    return result
+  }
+}
+
